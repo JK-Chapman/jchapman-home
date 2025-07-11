2fb7b0aceff00dc16a420c273c7b1e44650e69ec
Jon Chapman <jonkchpmn@gmail.com>
2025-07-10 20:15:54 -0400

    Rm node yml

--- app/components/greetingTile.tsx
+++ app/components/greetingTile.tsx
@@ -3,7 +3,7 @@ import photo from '../resources/photo.jpg';
 export function GreetingTile() {
   return (
     <div className="flex flex-col items-center">
-      <img className="rounded-full w-90 h-90 object-cover border-4 border-white-700 mb-4" alt="Profile" />
+      <img className="rounded-full w-90 h-90 object-cover border-4 border-white-700 mb-4" alt="Profile" src={photo} />
       {/* <h5 className="mb-1 text-xl font-bold text-white">Jonathan Chapman</h5> */}
       {/* <p className="mb-2 text-gray-400">Full Stack Developer</p> */}
     </div>
